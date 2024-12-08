adeb7f0fc2825375f87fad29460d50aa81f06097
gizmore <gizmore@wechall.net>
2024-12-08 09:18:18 +0100

    'fixes'

--- connector/Discord.py
+++ connector/Discord.py
@@ -4,6 +4,8 @@ import discord
 
 from gdo.base.Logger import Logger
 from gdo.base.Message import Message
+from gdo.base.Render import Mode
+from gdo.base.Util import Strings
 from gdo.core.Connector import Connector
 from gdo.core.GDO_User import GDO_User
 from gdo.discord.connector.DiscordClient import DiscordClient
@@ -13,6 +15,9 @@ class Discord(Connector):
 
     _client: DiscordClient
     _dog: GDO_User
+
+    def get_render_mode(self) -> Mode:
+        return Mode.TXT
 
     def gdo_connect(self):
         from gdo.discord.module_discord import module_discord
@@ -30,15 +35,21 @@ class Discord(Connector):
         return self._dog
 
     async def gdo_send_to_channel(self, message: Message):
+        text = message._result
         channel = message._env_channel
-        text = message._result[:4096]
         Logger.debug(f"{channel.render_name()} << {text}")
         prefix = f'{message._env_user.render_name()}: ' if not message._thread_user else ''
         text = f"{prefix}{text}"
-        await self._client.get_channel(int(channel.get_name())).send(text)
+        chan = self._client.get_channel(int(channel.get_name()))
+        chunks = Strings.split_boundary(text, 4096)
+        for chunk in chunks:
+            await chan.send(chunk)
 
     async def gdo_send_to_user(self, message: Message):
-        text = message._result[:4096]
+        text = message._result
         user = message._env_user
         Logger.debug(f"{user.render_name()} << {text}")
-        await self._client.get_user(int(user.get_name())).send(text)
+        usr = self._client.get_user(int(user.get_name()))
+        chunks = Strings.split_boundary(text, 4096)
+        for chunk in chunks:
+            await usr.send(chunk)
--- connector/DiscordClient.py
+++ connector/DiscordClient.py
@@ -4,6 +4,8 @@ from gdo.base.Application import Application
 from gdo.base.Message import Message
 from gdo.base.Render import Mode
 from gdo.core.GDO_Permission import GDO_Permission
+from gdo.core.GDO_Server import GDO_Server
+from gdo.core.GDO_Session import GDO_Session
 from gdo.core.GDO_UserPermission import GDO_UserPermission
 
 if TYPE_CHECKING:
@@ -21,23 +23,26 @@ class DiscordClient(discord.Client):
     def connector(self, connector: 'Discord'):
         self._connector = connector
         return self
-    
+
+    def get_server(self) -> GDO_Server:
+        return self._connector._server
+
     async def on_ready(self):
-        dog = self._connector._server.get_or_create_user(str(self.user.id), self.user.name)
+        Logger.debug("Connected to Discord!")
+        dog = self.get_server().get_or_create_user(str(self.user.id), self.user.name)
         GDO_UserPermission.grant(dog, GDO_Permission.ADMIN)
         GDO_UserPermission.grant(dog, GDO_Permission.STAFF)
         self._connector._dog = dog
 
     async def on_message(self, message):
         Application.tick()
-        serv = self._connector._server
+        serv = self.get_server()
         user = serv.get_or_create_user(str(message.author.id), message.author.display_name)
-        chan = None
-        if message.channel:
-            chan = serv.get_or_create_channel(str(message.channel.id), message.channel.name)
-        msg = Message(message.clean_content, Mode.TXT).env_user(user).env_server(serv).env_channel(chan)
-        await msg.execute()
-
-
-    def on_error(self, event_method: str, /, *args: Any, **kwargs: Any) -> None:
-        Logger.error(event_method)
+        if self._connector.gdo_get_dog_user() != user:
+            chan = None
+            if message.channel:
+                chan = serv.get_or_create_channel(str(message.channel.id), message.channel.name)
+            Logger.debug(f"{user.render_name()} >> {message.clean_content}")
+            msg = Message(message.clean_content, Mode.TXT).env_user(user).env_server(serv).env_channel(chan)
+            msg.env_session(GDO_Session.for_user(user))
+            await msg.execute()
