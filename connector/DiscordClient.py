e2e2cc5bdbd45d0a358c4839ef72a1bc947ca615
gizmore <gizmore@wechall.net>
2024-12-19 02:09:14 +0100

    'async fixes'

--- connector/DiscordClient.py
+++ connector/DiscordClient.py
@@ -30,7 +30,7 @@ class DiscordClient(discord.Client):
 
     async def on_ready(self):
         Logger.debug("Connected to Discord!")
-        dog = self.get_server().get_or_create_user(str(self.user.id), self.user.name)
+        dog = await self.get_server().get_or_create_user(str(self.user.id), self.user.name)
         dog.save_val('user_type', GDT_UserType.CHAPPY)
         GDO_UserPermission.grant(dog, GDO_Permission.ADMIN)
         GDO_UserPermission.grant(dog, GDO_Permission.STAFF)
@@ -39,12 +39,11 @@ class DiscordClient(discord.Client):
     async def on_message(self, message):
         Application.tick()
         serv = self.get_server()
-        user = serv.get_or_create_user(str(message.author.id), message.author.display_name)
+        user = await serv.get_or_create_user(str(message.author.id), message.author.display_name)
         if self._connector.gdo_get_dog_user() != user:
             chan = None
             if message.channel:
                 chan = serv.get_or_create_channel(str(message.channel.id), message.channel.name)
             Logger.debug(f"{user.render_name()} >> {message.clean_content}")
             msg = Message(message.clean_content, Mode.TXT).env_user(user).env_server(serv).env_channel(chan)
-            msg.env_session(GDO_Session.for_user(user))
             await msg.execute()
